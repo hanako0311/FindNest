55ccf801c77451c8aa1271a6ea8e08b86bf3cfb1
Hana <hanako0311>
2024-04-16 07:41:40 +0800

    profile ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux";
+import { FaSignOutAlt } from "react-icons/fa";
 
 export default function DashProfile() {
-  return <div>DashProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div
+          className="w-32 h-32 self-center cursor-pointershadow-md
+        overflow-hidden rounded-full"
+        >
+          <img
+            src={currentUser.profilePicture}
+            alt="profile picture"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="text"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button type="submit" gradientDuoTone="redToYellow" outline>
+          Update Profile
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-end mt-5 ">
+        <button
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          Sign Out
+          <FaSignOutAlt style={{ fontSize: "12px", marginLeft: "8px" }} />
+        </button>
+      </div>
+    </div>
+  );
 }
