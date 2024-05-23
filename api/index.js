e57cfed1b4a78555aadf042b5e88bbe09a5fe713
Hana <hanako0311>
2024-05-24 07:01:43 +0800

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import itemRoutes from "./routes/items.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 // Catch-all for unhandled routes
 app.use((req, res, next) => {
